f524b71200e6f4d34a79daf274eac07f4b4484fe
y00272684 <yanghaitao3@huawei.com>
2016-03-26 11:58:47 +0800

    函数式编程_filter

---
+++ 廖雪峰Python教程练习题代码/函数式编程/filter.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2016-3-26 
+
+@author: Aigoz
+'''
+#回数是指从左向右读和从右向左读都是一样的数，例如12321，909。请利用filter()滤掉非回数：
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+# 测试:
+output = filter(is_palindrome, range(1, 1000))
+print(list(output))
